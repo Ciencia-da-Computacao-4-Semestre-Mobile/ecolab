d7e28a00b88923a086d5489291a85dbafae0c95a
Guilherme <sou.guilhermedias@gmail.com>
2025-10-12 00:33:43 -0300

    implementação da Firebase iniciada

---
+++ app/build/generated/hilt/component_sources/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_example_ecolab_EcoLabApplication.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.example.ecolab.EcoLabApplication",
+    rootPackage = "com.example.ecolab",
+    originatingRoot = "com.example.ecolab.EcoLabApplication",
+    originatingRootPackage = "com.example.ecolab",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "EcoLabApplication",
+    originatingRootSimpleNames = "EcoLabApplication"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_example_ecolab_EcoLabApplication {
+}
--- app/build/tmp/kapt3/stubs/debug/com/example/ecolab/ui/screens/LoginScreenKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/ecolab/ui/screens/LoginScreenKt.java
@@ -1,19 +1,25 @@
 package com.example.ecolab.ui.screens;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.compose.foundation.layout.Arrangement;
 import androidx.compose.material3.ButtonDefaults;
+import androidx.compose.material3.SnackbarHostState;
 import androidx.compose.runtime.Composable;
 import androidx.compose.ui.Alignment;
 import androidx.compose.ui.Modifier;
 import androidx.compose.ui.text.input.PasswordVisualTransformation;
-import androidx.compose.ui.text.style.TextAlign;
+import com.example.ecolab.R;
 import com.example.ecolab.ui.theme.Palette;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u000e\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u001a\u0010\u0000\u001a\u00020\u00012\u0010\b\u0002\u0010\u0002\u001a\n\u0012\u0004\u0012\u00020\u0001\u0018\u00010\u0003H\u0007\u00a8\u0006\u0004"}, d2 = {"LoginScreen", "", "onLogin", "Lkotlin/Function0;", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u0014\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a \u0010\u0000\u001a\u00020\u00012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00010\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u0005H\u0007\u00a8\u0006\u0006"}, d2 = {"LoginScreen", "", "onLogin", "Lkotlin/Function0;", "viewModel", "Lcom/example/ecolab/ui/screens/LoginViewModel;", "app_debug"})
 public final class LoginScreenKt {
     
     @androidx.compose.runtime.Composable()
-    public static final void LoginScreen(@org.jetbrains.annotations.Nullable()
-    kotlin.jvm.functions.Function0<kotlin.Unit> onLogin) {
+    public static final void LoginScreen(@org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function0<kotlin.Unit> onLogin, @org.jetbrains.annotations.NotNull()
+    com.example.ecolab.ui.screens.LoginViewModel viewModel) {
     }
 }
